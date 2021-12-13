2d338d533b2e96afb8221a4f0d61d9224e5a7702
Wilgner Rafael Garcia <wilgner.rafael@gmail.com>
2021-12-13 13:14:29 -0300

    Update gke-cluster-init.sh

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -1,6 +1,6 @@
 # Creates 3 GKE autopilot clusters
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
-export PROJECT_ID="wrgarcia-poc-migrate"
+export PROJECT_ID="YOUR_PROJECT_ID"
 # Test cluster
 echo "creating testcluster..."
 gcloud beta container --project "$PROJECT_ID" clusters create-auto "testcluster" \
